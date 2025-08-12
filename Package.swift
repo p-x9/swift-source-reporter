426efe10eb8f9da02fcda00911f6a0c0816f7c54
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-08-12 20:36:44 +0900

    format

--- Package.swift
+++ Package.swift
@@ -1,21 +1,19 @@
-// swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
-    name: "SourceReporter",
+    name: "swift-source-reporter",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SourceReporter",
-            targets: ["SourceReporter"]),
+            targets: ["SourceReporter"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SourceReporter"),
+            name: "SourceReporter"
+        ),
         .testTarget(
             name: "SourceReporterTests",
             dependencies: ["SourceReporter"]
